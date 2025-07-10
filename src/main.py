6ce912a59a7501d07d1afa4b471d0d883425e733
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-07-10 16:51:50 +0530

    add message formatting

--- src/main.py
+++ src/main.py
@@ -1,11 +1,19 @@
+import sys
+
 import typer
+
+from utils import msg_utils
 
 app = typer.Typer()
 
 
 @app.command()
-def validate():
-    print("Valid")
+def validate(otp: int, secret: int):
+    if otp == secret:
+        msg_utils.print_success_msg("Valid OTP")
+    else:
+        msg_utils.print_error_msg("Invalid OTP")
+        sys.exit(1)
 
 
 def main():
