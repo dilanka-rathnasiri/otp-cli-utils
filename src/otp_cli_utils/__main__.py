899220bd7e8a1db07600835348e72c786ef7d7d0
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-08-10 18:05:36 +0530

    add help texts

--- src/otp_cli_utils/__main__.py
+++ src/otp_cli_utils/__main__.py
@@ -5,11 +5,20 @@ import typer
 from otp_cli_utils.services.otp_services import validate_otp
 from otp_cli_utils.utils import msg_utils
 
-app = typer.Typer()
+app = typer.Typer(
+    name="otp-cli-utils",
+    help="cli tool for OTP",
+)
 
 
-@app.command()
-def validate(otp: str, secret: str):
+@app.command(help="validate otp")
+def validate(
+    otp: str = typer.Argument(help="The OTP code to validate"),
+    secret: str = typer.Argument(help="OTP secret"),
+):
+    """
+    Validate if the provided OTP matches the expected value for the given secret
+    """
     if validate_otp(otp, secret):
         msg_utils.print_success_msg("Valid OTP")
     else:
