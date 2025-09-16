bd82060d4b828efb5f90c41958053ba2afa5dbd1
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-09-16 23:32:59 +0530

    add test cases for time window

--- src/tests/test_validate_otp.py
+++ src/tests/test_validate_otp.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 import pytest
 from pyotp import TOTP
 
@@ -30,3 +32,34 @@ def test_validate_invalid_otp(otp_secret):
     current_otp_code = "1234567"
 
     assert otp_services.validate_otp(otp_secret, current_otp_code, 0) is False
+
+
+def test_validate_otp_with_window_count_one_invalid_previous_otp(otp_secret):
+    """
+    Test the validate_otp function with a window count of 2 and an invalid OTP from the previous window
+    """
+    invalid_otp_code = "1234567"
+
+    assert otp_services.validate_otp(otp_secret, invalid_otp_code, 2) is False
+
+
+def test_validate_otp_with_window_count_two_valid_previous_otp(
+    otp_secret, totp_instance
+):
+    """
+    Test the validate_otp function with a window count of 2 and a valid OTP from two windows ago
+    """
+    previous_otp_code = totp_instance.at(datetime.now() - timedelta(seconds=60))
+
+    assert otp_services.validate_otp(otp_secret, previous_otp_code, 2) is True
+
+
+def test_validate_otp_with_window_count_one_valid_two_windows_ago_otp(
+    otp_secret, totp_instance
+):
+    """
+    Test the validate_otp function with a window count of 1 and a valid OTP from two windows ago
+    """
+    previous_otp_code = totp_instance.at(datetime.now() - timedelta(seconds=60))
+
+    assert otp_services.validate_otp(otp_secret, previous_otp_code, 1) is False
