84f74a8bb2fcd6ed71c70908086c2e176a6e2ddf
Dilanka Rathnasiri <rdilanka17@gmail.com>
2025-10-04 13:25:36 +0530

    add valid time option to cli tool

--- src/otp_cli_utils/services/otp_services.py
+++ src/otp_cli_utils/services/otp_services.py
@@ -20,11 +20,22 @@ def get_otp(secret: str) -> str:
 
 
 def get_otp_times_for_window_count(window_count: int) -> List[datetime]:
+    """
+    Get a list of past datetime objects corresponding with the given window count
+
+    The list will contain the current time and the previous window_count number of 30s time windows
+
+    Args:
+        window_count (int): The number of past 30s time windows to consider
+
+    Returns:
+        List[datetime]: A list of datetime objects representing the past window_count number of 30s time windows
+    """
     now = datetime.now()
     return [now - timedelta(seconds=30 * i) for i in range(window_count + 1)]
 
 
-def get_past_window_count_for_valid_time_period(time_period: int) -> int:
+def get_window_count_for_time_period(time_period: int) -> int:
     """
     Calculate the number of past 30s time windows for the given valid time period
 
@@ -48,14 +59,12 @@ def validate_otp(secret: str, otp_code: str, window_count: int) -> bool:
     Args:
         secret (str): The secret key to validate against
         otp_code (str): The OTP code to validate
+        window_count (int): The number of past 30s time window count to consider
 
     Returns:
         bool: True if the OTP code is valid, False otherwise
     """
     totp = TOTP(secret.upper())
-
-    if window_count == 0:
-        return validate_otp_at(totp, otp_code, datetime.now())
 
     otp_times = get_otp_times_for_window_count(window_count)
 
